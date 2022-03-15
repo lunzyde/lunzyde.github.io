131cbe6416497357a3a46b82ad5ceadf90d442fc
Bryan B. Bonilla Díaz <45500800+lunzyde@users.noreply.github.com>
2022-03-15 01:03:22 -0400

    Updated Pages + Added New Page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 /*
-	Escape Velocity by HTML5 UP
-	html5up.net | @ajlkn
+	Bryan B. Bonilla Díaz (lunzyde) - Portfolio [main.js]
+	
+	Template used: Escape Velocity by HTML5 UP
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 

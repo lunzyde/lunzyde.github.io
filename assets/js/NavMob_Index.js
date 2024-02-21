a1a4fc11536342e985fa53d0556ef669dd50e960
Bryan B. Bonilla Díaz <45500800+lunzyde@users.noreply.github.com>
2024-02-21 13:52:39 -0500

    Updating NavMob related .js files

--- assets/js/NavMob_Index.js
+++ assets/js/NavMob_Index.js
@@ -19,7 +19,7 @@
 					'<a class="link depth-0" href="games.html"><span class="indent-0"></span><i class="fas fa-gamepad" aria-hidden="true"></i> Games</a>' +
 					'<a class="link depth-0" href="prototypes.html"><span class="indent-0"></span><i class="fas fa-tools" aria-hidden="true"></i> Prototypes</a>' +
 					'<a class="link depth-0" href="audio.html"><span class="indent-0"></span><i class="fas fa-headphones" aria-hidden="true"></i> Audio</a>' +
-					'<a class="link depth-0" href="misc.html"><span class="indent-0"></span><i class="fas fa-briefcase" aria-hidden="true"></i> Work</a>' +
+					'<a class="link depth-0" href="misc.html"><span class="indent-0"></span><i class="fas fa-briefcase" aria-hidden="true"></i> Misc</a>' +
 					'<a class="link depth-0" href="cv.html"><span class="indent-0"></span><i class="fas fa-book" aria-hidden="true"></i> CV</a>' +
 					'<a class="link depth-0" href="about.html"><span class="indent-0"></span><i class="fas fa-user" aria-hidden="true"></i> About</a>' +
 				'</nav>' + '</div>'
